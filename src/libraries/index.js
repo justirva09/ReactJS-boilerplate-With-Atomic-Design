6cacb95cf7fdb51c761970551ffc406a77f12715
Muhammad Irva <48151470+justirva09@users.noreply.github.com>
2021-03-04 14:18:58 +0700

    add axios into libraries

--- src/libraries/index.js
+++ src/libraries/index.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase';
 import moment from 'moment';
+import axios from 'axios';
 
 export {
   React, 
   Component,
   PropTypes,
   firebase,
-  moment
+  moment,
+  axios
 };
 
 export * from 'react';
 export * from 'react-router-dom';
-export * from 'react-router-transition';
 export * from 'react-redux';
 export * from 'redux';
 export * from 'redux-thunk';
